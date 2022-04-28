e978696f86d3bbe93ded373a287493a7517a1719
juan-gimenez <juancitogimenez0@gmail.com>
2022-04-28 10:21:45 -0700

    fixed error of datetime

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,12 +13,12 @@ import os
 def do_pack():
     """ creates a q.tgz file"""
 
-    datetime = datetime.now().strftime("%Y%m%d%H%M%S")
+    datet = datetime.now().strftime("%Y%m%d%H%M%S")
     checkifdir = isdir("versions")
     if checkifdir is False:
         # if versions is not created create it
         local("mkdir versions")
-    f = "versions/web_static_{}.tgz".format(datetime)
+    f = "versions/web_static_{}.tgz".format(datet)
     local("tar -cvzf {} web_static".format(f))
     # return the archive path if the archive has been correctly generated
     if not (os.path.exists(f)):
