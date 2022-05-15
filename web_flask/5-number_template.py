02d4f71b9d1c21d441a80d67651e86c029a6128e
Juan Gimenez <juancitogimenez0@gmail.com>
2022-05-15 19:16:42 +0000

    task 5 wrong

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -43,12 +43,10 @@ def integer(n):
     return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def displayhtml(n):
-    """
-    display a HTML page only if n is an integer
-    """
-    return render_template("5-number.html", n=n)
+    '''display a HTML page only if n is an integer'''
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
